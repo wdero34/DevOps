9b5d8fbf47c5405b756419d8bf012036f3eea19a
wdero34 <118142112+wdero34@users.noreply.github.com>
2022-11-13 18:17:56 -0600

    Add files via upload

---
+++ extracredithypo.py
@@ -0,0 +1,6 @@
+from math import sqrt
+print("Enter length of sides A and B:")
+a = float(input("A: "))
+b = float(input("B: "))
+c = sqrt(a**2 + b**2)
+print("The hypotenuse is:", c )
---
+++ fibonacciextra.py
@@ -0,0 +1,13 @@
+import fibo
+#program prints out fib series until it lands upon number user inputs
+b = int(input("Input number you want program to stop on: "))
+a = 0
+c = 1
+counter = 1 
+total = 0
+print("Fibonacci : ", stop = " ")
+while(counter <= b):
+    print(sum, stop = " ")
+    coutner += 1
+    c = total 
+    total  = a + c
